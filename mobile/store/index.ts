d766da75650197be6a7ea16937ccdb41ee7a0563
Madhav Sharma <madhava010801@gmail.com>
2023-12-05 11:40:46 -0800

    Added Redux

---
+++ mobile/store/index.ts
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore, Store } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer, { RootState } from './reducers';
+
+const store: Store<RootState> = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+);
+
+export default store;
